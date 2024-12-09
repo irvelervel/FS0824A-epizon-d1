0940d1fd09b2d824b4a126011b8ce06c47513927
Stefano Casasola <ste.casasola@gmail.com>
2024-12-09 11:49:49 +0100

    Adds Redux implementation

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,1 +1,33 @@
 // qui andremo a scrivere la nostra funzione Reducer
+// la funzione reducer serve a calcolare il nuovo stato dell'app ogni volta che una action viene "dispatchata"
+// il reducer è in grado di calcolare il nuovo stato grazie ai due parametri con i quali viene invocato: lo stato attuale dell'app ("state") e la più recente action "dispatchata" (action)
+
+// l'unica cosa che ci manca a questo punto è lo STATO INIZIALE di Redux
+// al momento ci interessa salvare il contenuto del carrello dei libri!
+
+const initialState = {
+  // dividiamo per ragioni organizzative lo state in SLICES (fette)
+  cart: {
+    content: [], // all'inizio non abbiamo libri nel carrello
+  },
+}
+
+// alla prima invocazione di mainReducer (che avviene al caricamento della pagina) state è undefined (non esiste ancora!); diamo come valore di default del parametro state il nostro oggetto "initialState"
+const mainReducer = (state = initialState, action) => {
+  // mainReducer calcolerà lo stato nuovo
+  // quindi ritornerà SEMPRE un oggetto, il nuovo stato dell'app
+
+  // mainReducer è una PURE FUNCTION, non può modificare i propri parametri e non può eseguire "side-effects" (ad es. contattare delle API)
+
+  switch (action.type) {
+    // qui inseriamo tutte le possibili casistiche
+    // case blabla1
+    // case blabla2
+    default:
+      // finiamo qui dentro se l'action.type non corrisponde a nessuno dei case stabiliti precedentemente
+      return state
+    // nel peggiore dei casi ritorniamo lo stato esattamente come l'abbiamo trovato
+  }
+}
+
+export default mainReducer
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,1 +1,9 @@
-// qui dentro utilizzeremo la libreria @reduxjs/toolkit per inizializzare lo store di Redux
+import { configureStore } from '@reduxjs/toolkit'
+import mainReducer from '../reducers'
+// poichè il file dentro la cartella reducers si chiama "index.js" non è necessario specificarlo alla riga 2
+
+const myReduxStore = configureStore({
+  reducer: mainReducer,
+})
+
+export default myReduxStore
